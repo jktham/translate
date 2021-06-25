236b8ea316c9d6941ea1c338f82f6331bf6669b1
jktham-pc <jonas.tham@gmx.ch>
2021-06-25 20:26:17 +0200

    implement tesseract ocr, still buggy

--- convert.bat
+++ convert.bat
@@ -1,7 +1,7 @@
 @echo off
 echo starting conversion
 
-for /l %%x in (1, 1, 11) do echo processing %%x & ("C:/Program Files/Tesseract-OCR/tesseract" C:/Users/Jonas/translate/%%x.jpg stdout 1>> C:/Users/Jonas/translate/output1.txt -l ita)
+for /l %%x in (0, 1, 1) do echo processing %%x & ("C:/Program Files/Tesseract-OCR/tesseract" C:/Users/Jonas/projects/translate/input/%%x.jpg stdout 1>> C:/Users/Jonas/projects/translate/output/result.txt -l ita --psm 6)
 
 echo done
 pause
--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 import pytesseract
-import PIL
-import os
+import cv2
+import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract"
 
-text = pytesseract.image_to_string(PIL.Image.open('input/0.jpg'), lang="ita")
+def process_image(n):
+    img = cv2.imread('input/' + str(n) + '.jpg')
+    img = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+    img = cv2.convertScaleAbs(img, alpha=1, beta=0)
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+    img = cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    return img
 
-print(pytesseract.image_to_osd(PIL.Image.open('input/0.jpg')))
 
-f = open("output/result.txt", "a")
-f.write(text)
-f.close()
+def display_image(img):
+    cv2.namedWindow("output", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("output", 800, 1000)
+    cv2.imshow("output", img)
+    cv2.waitKey()
+    return
 
+
+def tesseract(img):
+    text = pytesseract.image_to_string(img, lang="ita")
+    return text
+
+
+def process_text(text):
+    text = text.replace("\n", " ")
+    text = text.replace("\f", "\n\n")
+    text = text.replace("- ", "")
+    return text
+
+
+def output(text):
+    f = open("output/result.txt", "a")
+    f.write(text)
+    f.close()
+    return
+
+
+for i in range(0, 10):
+    # display_image(process_image(i))
+    output(process_text(tesseract(process_image(i))))
