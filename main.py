aabe91adf6fbb2104828ea18bb6683864d073fac
jktham-pc <jonas.tham@gmx.ch>
2021-06-25 18:21:48 +0200

    add new images

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pytesseract
 import PIL
+import os
 
 pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract"
 
-print(pytesseract.image_to_string(PIL.Image.open('input/test.png'), lang="eng"))
+text = pytesseract.image_to_string(PIL.Image.open('input/0.jpg'), lang="ita")
+
+print(pytesseract.image_to_osd(PIL.Image.open('input/0.jpg')))
+
+f = open("output/result.txt", "a")
+f.write(text)
+f.close()
+
